01c433dc8c9d71d7f163edfa1a51c58e8635b061
Bertrand Picard <bertrand.picard@kujjuk.com>
2022-04-07 13:28:11 +0200

    Updates

---
+++ precache-manifest.b7abd51845ad1e1c7c8844d9a5eac094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225ea7ea02f1ad3ddbb2596c5f622f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b7b3ded75ac95630766",
+    "url": "/static/css/2.e4c729c3.chunk.css"
+  },
+  {
+    "revision": "f4fb5cf92733151264db",
+    "url": "/static/css/main.319cc243.chunk.css"
+  },
+  {
+    "revision": "1b7b3ded75ac95630766",
+    "url": "/static/js/2.8ebf0080.chunk.js"
+  },
+  {
+    "revision": "2821a807f373b962ca3ae8bc13db778f",
+    "url": "/static/js/2.8ebf0080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4fb5cf92733151264db",
+    "url": "/static/js/main.322906f2.chunk.js"
+  },
+  {
+    "revision": "861e4233396253aee4bd",
+    "url": "/static/js/runtime-main.07fa7236.js"
+  }
+]);
